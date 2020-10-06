78bb794776300185f8deb473060d210a558e7cd4
rednibia <andrewaibinder@gmail.com>
2020-10-06 09:19:37 -0400

    use SoCo to start music

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ import time
 import paho.mqtt.client as mqtt
 
 
+mqttBroker = "127.0.0.1"
+mqttPort = 1883
+topic = "music/playlist"
+
+
 def on_message(client, userdata, message):
     print("received message: ", str(message.payload.decode("utf-8")))
 
 
-mqttBroker = "127.0.0.1"
-client = mqtt.Client("Music Relay")
-client.connect(mqttBroker)
+def on_connect(client, userdata, flags, rc):
+    print("Connected to {0} with result code {1}".format(mqttBroker, rc))
+    client.subscribe(topic)
 
-client.loop_start()
-client.subscribe("music/playlist")
-client.on_message = on_message
-time.sleep(4) # wait
-client.loop_stop() #stop the loop
+
+def main():
+    client = mqtt.Client("Music Relay")
+    client.connect(mqttBroker)
+    client.loop_forever()
+    client.subscribe(topic)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+
+if __name__ == "__main__":
+    main()
